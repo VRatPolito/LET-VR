33a758c2f883a741925b118c6b9fc90c922faeab
F. Gabiele Pratticò <ilmatematic@gmail.com>
2019-11-18 16:01:04 +0100

    dragonball arena & drone with panel

---
+++ Assets/DroneWithPanelController.cs
@@ -0,0 +1,57 @@
+﻿
+/*
+ * Custom template by F. Gabriele Pratticò {filippogabriele.prattico@polito.it}
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class DroneWithPanelController : MonoBehaviour
+{
+	#region Events
+		
+	#endregion
+	
+	#region Editor Visible
+		
+	#endregion
+	
+	#region Private Members and Constants
+
+    private MIPanelController _panelController;
+
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region MonoBehaviour
+
+    void Awake()
+    {
+
+        _panelController = GetComponentInChildren<MIPanelController>();
+        Assert.IsNotNull(_panelController);
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    #endregion
+
+    #region Helper Methods
+
+    #endregion
+
+    #region Events Callbacks
+
+    #endregion
+
+    #region Coroutines
+
+    #endregion
+
+}
