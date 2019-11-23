bcdcc0f532e3d4432f03d94a66b41f6500f86186
CrystalDragon <crystal_dragon@hotmail.it>
2019-11-23 12:22:12 +0100

    nuovo task manipulation, manca ancora logger

--- Assets/Scripts/GrabDestination.cs
+++ Assets/Scripts/GrabDestination.cs
@@ -5,24 +5,51 @@ using UnityEngine;
 
 public class GrabDestination : Destination
 {
-    [SerializeField] private List<GenericItem> _items;
+    [SerializeField] private GenericItem _item;
     [SerializeField] private bool _autoDisable = true;
-    [SerializeField] private bool _makeItemsUngrabbable = true;
+    [SerializeField] private bool _makeItemUngrabbable = true;
     [SerializeField] private bool _makeKinematic = false;
+    [SerializeField] private float _timeStillForStop = 2;
+    private bool _itemInRange;
+    private float _timeStop = 0;
+    private Vector3 _lastPos;
+    private Quaternion _lastRot;
 
-    private void OnTriggerEnter(Collider c)
+    private void OnTriggerEnter(Collider other)
     {
-		var item = c.GetComponent<GenericItem>();
-        if (item != null && _items.Contains(item))
+		var item = other.GetComponent<GenericItem>();
+        if (item == _item)
         {
-			var p = item.Player;
-			if(_makeKinematic)
-				item.IsKinematic = true;
-            if(p != null)
-			    p.DropItem(c.transform, true);
-            if(_makeItemsUngrabbable)
-				item.CanInteract(false, LocomotionManager.Instance.CurrentPlayerController.GetComponent<VRItemController>());
-            if(_autoDisable)
+            _itemInRange = true;
+            _timeStop = Time.time + _timeStillForStop;
+            _lastPos = _item.transform.position;
+            _lastRot = _item.transform.rotation;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        var item = other.GetComponent<GenericItem>();
+        if (item == _item)
+        {
+            _itemInRange = false;
+            _timeStop = 0;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(_item.transform.position != _lastPos || _item.transform.rotation != _lastRot)
+            _timeStop = Time.time + _timeStillForStop;
+        else if (Time.time == _timeStop)
+        {
+            var p = _item.Player;
+            if (_makeKinematic)
+                _item.IsKinematic = true;
+            if (p != null)
+                p.DropItem(_item.transform, true);
+            if (_makeItemUngrabbable)
+                _item.CanInteract(false, LocomotionManager.Instance.CurrentPlayerController.GetComponent<VRItemController>());
+            if (_autoDisable)
                 gameObject.SetActive(false);
         }
     }
